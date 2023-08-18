f5cbd428735f1bd70eb9293bcf3217447af0b33b
Gabriel Ferreira <gabrielffguimaraes@gmail.com>
2023-08-18 02:34:41 -0300

    update

--- authuser/src/main/java/com/ead/authuser/configs/RestTemplateApi.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestTemplateApi.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.Course;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,9 @@ import java.util.UUID;
 @Component
 @Slf4j
 public class RestTemplateApi {
+
+    @Value("${ead.api.url.course}")
+    private String urlCourseApi;
     @Autowired
     RestTemplate restTemplate;
 
@@ -25,7 +29,7 @@ public class RestTemplateApi {
     public List<Course> getAllCoursesByUser(UUID userId,String token) throws HttpStatusCodeException {
 
         //String url = "http://localhost:8080/ead-course/api/courseUser/users/"+userId+"/courses";
-        String url = "http://localhost:8082/ead-course/api/courseUser/users/4e339be5-4ea9-4eb0-9faa-f8b8d7a466de/courses";
+        String url = urlCourseApi+"/api/courseUser/users/4e339be5-4ea9-4eb0-9faa-f8b8d7a466de/courses";
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("Authorization",token);
         httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
