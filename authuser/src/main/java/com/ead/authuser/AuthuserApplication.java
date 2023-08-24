b85a983ae8b15dca742b192dfa023b8b649368be
Gabriel Ferreira <gabrielffguimaraes@gmail.com>
2023-08-24 04:55:46 -0300

    elastic search configurations

--- authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -1,16 +1,32 @@
 package com.ead.authuser;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@Slf4j
 @SpringBootApplication
 @EnableFeignClients
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthuserApplication {
+	@Value("${teste}")
+	private String teste;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthuserApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+			log.info(teste);
+		};
 	}
 	// teste
 }
